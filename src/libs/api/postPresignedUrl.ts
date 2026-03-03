350eaea1c48c956e86c8b7366f09aa8f2165b1e4
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2026-03-03 22:08:49 +0900

    fix: postPresignedUrl에 Bearer Token 헤더 추가

--- src/libs/api/postPresignedUrl.ts
+++ src/libs/api/postPresignedUrl.ts
@@ -1,11 +1,21 @@
 import axios from 'axios'
+import { STORAGE_KEY } from '../constants/auth'
 import { ApiResponseT } from '../types/api.types'
 import { PostPresignedURLRequestT, PostPresignedURLResponseT } from '../types/uploadImage'
+import { getToken } from '../utils/handleSecureStorage'
 
 export const postPresignedUrl = async ({ extension }: PostPresignedURLRequestT) => {
+  const accessToken = await getToken(STORAGE_KEY.ACCESS_TOKEN)
+  if (!accessToken) throw new Error('[AUTH] access token is missing')
+
   const { data } = await axios.post<ApiResponseT<PostPresignedURLResponseT>>(
     `${process.env.EXPO_PUBLIC_API_URL}:8081/images/presigned-url`,
-    { extension }
+    { extension },
+    {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    }
   )
   return data.data
 }
