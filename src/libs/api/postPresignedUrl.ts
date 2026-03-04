5c18bc1046fd427a494a0ba4c038aa53f9fa809d
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2026-03-04 21:40:48 +0900

    refactor: 데이터 패칭 시 axios 대신 fetch 사용

--- src/libs/api/postPresignedUrl.ts
+++ src/libs/api/postPresignedUrl.ts
@@ -1,21 +1,23 @@
-import axios from 'axios'
 import { STORAGE_KEY } from '../constants/auth'
 import { ApiResponseT } from '../types/api.types'
-import { PostPresignedURLRequestT, PostPresignedURLResponseT } from '../types/uploadImage'
+import { PostPresignedURLRequestT, PostPresignedURLResponseT } from '../types/uploadImage.types'
 import { getToken } from '../utils/handleSecureStorage'
 
 export const postPresignedUrl = async ({ extension }: PostPresignedURLRequestT) => {
   const accessToken = await getToken(STORAGE_KEY.ACCESS_TOKEN)
   if (!accessToken) throw new Error('[AUTH] access token is missing')
 
-  const { data } = await axios.post<ApiResponseT<PostPresignedURLResponseT>>(
-    `${process.env.EXPO_PUBLIC_API_URL}:8081/images/presigned-url`,
-    { extension },
-    {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    }
-  )
-  return data.data
+  const response = await fetch(`${process.env.EXPO_PUBLIC_API_URL}:8080/images/presigned-url`, {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ extension }),
+  })
+
+  if (!response.ok) throw new Error(`[IMAGE_UPLOAD] presigned-url 요청 실패: ${response.status}`)
+
+  const json: ApiResponseT<PostPresignedURLResponseT> = await response.json()
+  return json.data
 }
--- src/libs/api/putImageToS3.ts
+++ src/libs/api/putImageToS3.ts
@@ -1,5 +1,9 @@
-import axios from 'axios'
-
 export const putImageToS3 = async (presignedUrl: string, image: Blob, mimeType: string) => {
-  await axios.put(presignedUrl, image, { headers: { 'Content-Type': mimeType } })
+  const response = await fetch(presignedUrl, {
+    method: 'PUT',
+    headers: { 'Content-Type': mimeType },
+    body: image,
+  })
+
+  if (!response.ok) throw new Error('[IMAGE_UPLOAD] 이미지 업로드 실패')
 }
