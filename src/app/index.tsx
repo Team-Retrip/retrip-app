2281ffe9d0474bc044c4256cdbec5fc0f8041a3a
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2026-03-12 21:15:54 +0900

    feat: Input 포커스 시 input 높이만큼 화면 길이 줄어들도록 수정

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,7 @@
 import { useGetWebviewMessage } from '@/libs/hooks/useGetWebviewMessage'
 import { WebBridge } from '@/libs/utils/sendMessageToWeb'
 import { useEffect, useRef } from 'react'
+import { KeyboardAvoidingView, Platform } from 'react-native'
 import Webview, { WebView } from 'react-native-webview'
 
 export default function Page() {
@@ -13,15 +14,21 @@ export default function Page() {
 
   return (
     // REF: https://github.com/react-native-webview/react-native-webview/blob/5bc526fce5b9d6225df183bdf3d8cf542007d90a/docs/Reference.md
-    <Webview
-      ref={webviewRef}
-      className="flex-1"
-      source={{ uri: 'https://www.naver.com' }}
-      onMessage={onMessage}
-      allowsBackForwardNavigationGestures
-      cacheEnabled
-      webviewDebuggingEnabled
-      startInLoadingState
-    />
+    <KeyboardAvoidingView className="flex-1 bg-white" behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+      <Webview
+        ref={webviewRef}
+        className="flex-1"
+        /**
+         * - ios simulator 사용 시: `http://localhost:3000`
+         * - 실기기 사용 시: LAN IP 주소 ex) `http://192.0.0.1:3000`
+         */
+        source={{ uri: 'http://192.168.45.186:3000/trip' }}
+        onMessage={onMessage}
+        allowsBackForwardNavigationGestures
+        cacheEnabled
+        webviewDebuggingEnabled
+        startInLoadingState
+      />
+    </KeyboardAvoidingView>
   )
 }
